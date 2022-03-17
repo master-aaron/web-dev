57aa9c6742a80eba4dd84db8fc7a2d21ab9588c2
Aaron Master <aaron@Aarons-MacBook-Pro.local>
2022-03-16 20:37:50 -0400

    dynamic stuff

--- src/components/Tuiter/navigation-sidebar/index.js
+++ src/components/Tuiter/navigation-sidebar/index.js
@@ -8,14 +8,16 @@ const NavigationSidebar = ({ active = "explore" }) => {
           <i className="fab fa-twitter"></i>
         </a>
         <a
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "home" ? "active" : ""}`}
           href="home.html"
         >
           <i className="fa fa-home"></i>
           Home
         </a>
         <a
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "home" ? "active" : ""}`}
           href="home.html"
         >
           <i className="fa fa-home"></i>
@@ -23,92 +25,106 @@ const NavigationSidebar = ({ active = "explore" }) => {
 
         <a
           href="explore.html"
-          className="list-group-item list-group-item-action active d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "explore" ? "active" : ""}`}
         >
           <i className="fa fa-hashtag"></i>
           Explore
         </a>
         <a
           href="explore.html"
-          className="list-group-item list-group-item-action active d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "explore" ? "active" : ""}`}
         >
           <i className="fa fa-hashtag"></i>
         </a>
         <a
           href="notifications.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "notifications" ? "active" : ""}`}
         >
           <i className="fa fa-bell"></i>
           Notifications
         </a>
         <a
           href="notifications.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "notifications" ? "active" : ""}`}
         >
           <i className="fa fa-bell"></i>
         </a>
         <a
           href="messages.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "messages" ? "active" : ""}`}
         >
           <i className="fa fa-envelope"></i>
           Messages
         </a>
         <a
           href="messages.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "messages" ? "active" : ""}`}
         >
           <i className="fa fa-envelope"></i>
         </a>
         <a
           href="bookmarks.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "bookmarks" ? "active" : ""}`}
         >
           <i className="fa fa-bookmark"></i>
           Bookmarks
         </a>
         <a
           href="bookmarks.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "bookmarks" ? "active" : ""}`}
         >
           <i className="fa fa-bookmark"></i>
         </a>
         <a
           href="lists.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "lists" ? "active" : ""}`}
         >
           <i className="fa fa-list"></i>
           Lists
         </a>
         <a
           href="lists.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "lists" ? "active" : ""}`}
         >
           <i className="fa fa-list"></i>
         </a>
         <a
           href="profile.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "profile" ? "active" : ""}`}
         >
           <i className="fa fa-person-booth"></i>
           Profile
         </a>
         <a
           href="profile.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "profile" ? "active" : ""}`}
         >
           <i className="fa fa-person-booth"></i>
         </a>
         <a
           href="more.html"
-          className="list-group-item list-group-item-action d-none d-xl-block d-xxl-block"
+          className={`list-group-item list-group-item-action d-none d-xl-block d-xxl-block
+          ${active === "more" ? "active" : ""}`}
         >
           <i className="fa fa-circle-notch"></i>
           More
         </a>
         <a
           href="more.html"
-          className="list-group-item list-group-item-action d-xl-none d-xl-none"
+          className={`list-group-item list-group-item-action d-xl-none d-xl-none
+          ${active === "more" ? "active" : ""}`}
         >
           <i className="fa fa-circle-notch"></i>
         </a>
